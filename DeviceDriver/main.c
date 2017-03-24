72ad16a7956f589f0e95665f1993948165e22c68
Robin Schiro <robinschiro@gmail.com>
2017-03-24 14:28:43 -0400

    Created basic structure of char device driver

--- DeviceDriver/main.c
+++ DeviceDriver/main.c
@@ -1,7 +1,23 @@
-#include <stdio.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#define  DEVICE_NAME "Sample"
+#define  CLASS_NAME  "CharDevice"
 
-int main(int argc, char *argv[])
+/** Function Prototypes **/
+int init_module(void);
+void cleanup_module(void);
+
+int init_module(void)
 {
-   printf("Hello World!\n");
+   printk(KERN_INFO "Installing");
    return 0;
+}
+
+void cleanup_module(void)
+{
+   printk(KERN_INFO "Removing");
 }
