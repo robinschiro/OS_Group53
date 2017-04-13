8cfcd1f25b124746e5cacc130f7f7ce073354d22
Robin Schiro <robinschiro@gmail.com>
2017-04-13 19:57:30 -0400

    Create inputDevice.c to serve as a write device.
    - Implemented mutex to guard critical sections.
    
    Signed-off-by: Robin Schiro <robinschiro@gmail.com>

--- SharedMemoryDeviceDriver/inputDevice.c
+++ SharedMemoryDeviceDriver/inputDevice.c
@@ -1,6 +1,6 @@
 /*
  * This character device driver allows a user program to write up to a certain number
- * of bytes to a buffer and to read those bytes FIFO-style from the buffer.
+ * of bytes to a buffer.
  * The implementations of most of the functions are based on code examples from the
  * following two sources:
  * http://www.tldp.org/LDP/lkmpg/2.6/html/x569.html
@@ -15,9 +15,10 @@
 #include <linux/unistd.h>
 #include <linux/string.h>
 #include <asm/uaccess.h>
+#include <linux/mutex.h>
 
 /** Constants **/
-#define DEVICE_NAME "SampleCharDevice"
+#define DEVICE_NAME "SampleInputDevice"
 #define BUFFER_SIZE 1024
 
 /** Function Prototypes **/
@@ -41,11 +42,14 @@ static struct file_operations fops =
    .write = device_write
 };
 
-/** Global variables **/
+/** Private Global variables **/
 
 static int majorVersion;
-static char buffer[BUFFER_SIZE + 1];
 static int remainingSpace = BUFFER_SIZE;
+
+/** Public Global variables **/
+char buffer[BUFFER_SIZE + 1];
+DEFINE_MUTEX(charDeviceMutex);
 
 /** Function Definitions **/
 
@@ -66,12 +70,18 @@ int init_module(void)
 
    // Initialize buffer.
    strcpy(buffer, "");
+   
+   // Initialize the mutex.
+   mutex_init(&charDeviceMutex);
 
    return 0;
 }
 
 void cleanup_module(void)
 {
+   // Destroy the mutex.
+   mutex_destroy(&charDeviceMutex);
+   
    // Deregister the device.
    unregister_chrdev(majorVersion, DEVICE_NAME);
 
@@ -81,6 +91,13 @@ void cleanup_module(void)
 
 static int device_open(struct inode* inodep, struct file* filep)
 {
+   // Lock the device to prevent it from being read from while it's being written to.
+   if (!mutex_trylock(&charDeviceMutex))
+   {
+      printk(KERN_INFO "This device is currently locked by another process.");
+      return -EBUSY;
+   }
+   
    // Increment the counter that is keeping track of the number of processes
    // that have opened the device. This is needed to prevent someone from 
    // uninstalling the module if it is still in use.
@@ -92,6 +109,9 @@ static int device_open(struct inode* inodep, struct file* filep)
 
 static int device_release(struct inode* inodep, struct file* filep)
 {
+   // Unlock the mutex.
+   mutex_unlock(&charDeviceMutex);
+   
    // Decrement the process usage counter.
    module_put(THIS_MODULE);
 
@@ -100,38 +120,10 @@ static int device_release(struct inode* inodep, struct file* filep)
 }
 
 static ssize_t device_read(struct file* filep, char* output, size_t length, loff_t* offset)
-{
-   // Functions like 'cat' will continue reading until 0 is returned as the output size.
-   // Therefore, return 0 if the buffer contents have already been sent to the user.
-   if (*offset > 0)
-   {
-       return 0;
-   }
+{   
+   printk(KERN_INFO "Error: Cannot read from an input device\n");
 
-   // Determine number of bytes to pop from buffer.
-   int numBytesToPop = (length < BUFFER_SIZE - remainingSpace) ? (length) : (BUFFER_SIZE - remainingSpace);
-
-   // Send the portion of the buffer contents to the user.
-   int result = copy_to_user(output, buffer, numBytesToPop);
-
-   // Overwrite the popped bytes with the remaining bytes in the buffer.
-   char temp[BUFFER_SIZE - numBytesToPop];
-   strcpy(temp, &buffer[numBytesToPop]);
-   strcpy(buffer, temp);
-
-   // Update the offset in order to indicate to the user program that the
-   // reading of the buffer should end.
-   *offset += numBytesToPop;
-
-   // Update number of remaining bytes.
-   remainingSpace += numBytesToPop;
-
-   // Log the fact that the device was read from.
-   printk(KERN_INFO "Buffer contents read from character device. Length requested: %d\n", length);
-   printk(KERN_INFO "Buffer contents after read: %s\n", buffer);
-
-   // Return the size of the buffer contents.
-   return numBytesToPop;
+   return -1;
 }
 
 static ssize_t device_write(struct file* filep, const char* message, size_t length, loff_t* offset)
--- SharedMemoryDeviceDriver/test_drivers.sh
+++ SharedMemoryDeviceDriver/test_drivers.sh
@@ -7,11 +7,13 @@ set -e
 source assert.sh
 
 # Set constants
-DEVICE_FILE_PATH=/dev/SampleCharDevice
+INPUT_DEVICE_FILE_PATH=/dev/SampleInputDevice
+OUTPUT_DEVICE_FILE_PATH=/dev/SampleOutputDevice
 
 echo "Removing the previous versions of module..."
 # Continue script even if the following command produce an error.
-rmmod main 2>/dev/null || true
+rmmod inputDevice 2>/dev/null || true
+#~ rmmod outputDevice 2>/dev/null || true
 
 echo "Building character device driver..."
 make
@@ -19,43 +21,46 @@ make
 # If the following fails, it likely means that the previous version of the module 
 # was not removed because a process was using it.
 echo "Installing the module..."
-insmod main.ko
+insmod inputDevice.ko
+#~ insmod outputDevice.ko
 
 # Retrieve the major version of the module from the last line of the kernel module log file.
 MAJOR_VERSION=$(dmesg | tail -1 | awk '{ print $NF }')
 
 echo "Creating the device file for major version ${MAJOR_VERSION}..."
-rm ${DEVICE_FILE_PATH} 2>/dev/null || true
-mknod ${DEVICE_FILE_PATH} c ${MAJOR_VERSION} 0
+rm ${INPUT_DEVICE_FILE_PATH} 2>/dev/null || true
+#~ rm ${OUTPUT_DEVICE_FILE_PATH} 2>/dev/null || true
+mknod ${INPUT_DEVICE_FILE_PATH} c ${MAJOR_VERSION} 0
+#~ mknod ${OUTPUT_DEVICE_FILE_PATH} c ${MAJOR_VERSION} 0
 
 printf "\nTest Results:\n"
 
 # Test 1: Basic Functionality
 # Push some data into the character device buffer.
 # Verify that popping each piece of data works correctly.
-echo -n "onetwothree" > ${DEVICE_FILE_PATH}
-assert "head -c 3 ${DEVICE_FILE_PATH}" "one"
-assert "head -c 3 ${DEVICE_FILE_PATH}" "two"
-assert "head -c 5 ${DEVICE_FILE_PATH}" "three"
-echo -n "five" > ${DEVICE_FILE_PATH}
-echo -n "six" > ${DEVICE_FILE_PATH}
-echo -n "seven" > ${DEVICE_FILE_PATH}
-assert "head -c 4 ${DEVICE_FILE_PATH}" "five"
-assert "head -c 3 ${DEVICE_FILE_PATH}" "six"
-assert "head -c 5 ${DEVICE_FILE_PATH}" "seven"
-echo -n "!@#% %^& 45670\$ >?:\"{}+_)|" > ${DEVICE_FILE_PATH}
-assert "head -c 8 ${DEVICE_FILE_PATH}" "!@#% %^&"
-assert "head -c 18 ${DEVICE_FILE_PATH}" " 45670\$ >?:\"{}+_)|"
-assert "cat ${DEVICE_FILE_PATH}" ""
+echo -n "onetwothree" > ${INPUT_DEVICE_FILE_PATH}
+#~ assert "head -c 3 ${OUTPUT_DEVICE_FILE_PATH}" "one"
+#~ assert "head -c 3 ${OUTPUT_DEVICE_FILE_PATH}" "two"
+#~ assert "head -c 5 ${OUTPUT_DEVICE_FILE_PATH}" "three"
+echo -n "five" > ${INPUT_DEVICE_FILE_PATH}
+echo -n "six" > ${INPUT_DEVICE_FILE_PATH}
+echo -n "seven" > ${INPUT_DEVICE_FILE_PATH}
+#~ assert "head -c 4 ${OUTPUT_DEVICE_FILE_PATH}" "five"
+#~ assert "head -c 3 ${OUTPUT_DEVICE_FILE_PATH}" "six"
+#~ assert "head -c 5 ${OUTPUT_DEVICE_FILE_PATH}" "seven"
+echo -n "!@#% %^& 45670\$ >?:\"{}+_)|" > ${INPUT_DEVICE_FILE_PATH}
+#~ assert "head -c 8 ${OUTPUT_DEVICE_FILE_PATH}" "!@#% %^&"
+#~ assert "head -c 18 ${OUTPUT_DEVICE_FILE_PATH}" " 45670\$ >?:\"{}+_)|"
+#~ assert "cat ${OUTPUT_DEVICE_FILE_PATH}" ""
 assert_end basic_functionality
 
 # Test 2: Read Overflow
 # Push some data into the character device buffer
 # and attempt to read more bytes than are in the buffer.
 # The read result should simply return the entire contents of the buffer.
-echo -n "The quick brown fox jumps over the lazy dog" > ${DEVICE_FILE_PATH}
-assert "head -c 500 ${DEVICE_FILE_PATH}" "The quick brown fox jumps over the lazy dog"
-assert "cat ${DEVICE_FILE_PATH}" ""
+echo -n "The quick brown fox jumps over the lazy dog" > ${INPUT_DEVICE_FILE_PATH}
+#~ assert "head -c 500 ${DEVICE_FILE_PATH}" "The quick brown fox jumps over the lazy dog"
+#~ assert "cat ${OUTPUT_DEVICE_FILE_PATH}" ""
 assert_end read_overflow
 
 # Test 3: Write Overflow
@@ -64,8 +69,8 @@ assert_end read_overflow
 # Verify that only the first 1024 bytes of the input was written to the device.
 SAMPLE_TEXT_INPUT="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus euismod ligula efficitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque molestie libero interdum auctor condimentum. Nullam non enim libero. Fusce fermentum lacus ex, non vehicula urna laoreet ut. Aenean at velit odio. Donec blandit imperdiet nunc, et molestie mi tempor at. Mauris dapibus leo augue. In ex ex, interdum ornare auctor sit amet, rhoncus ut ipsum. Integer dictum est non ornare scelerisque. Duis faucibus nisi accumsan, ullamcorper risus et, tincidunt dolor. Fusce laoreet ex purus, eu mattis urna pharetra at. Aliquam nec fermentum eros. Vivamus ac sapien eu metus euismod varius vel sit amet mi. Donec consectetur, tortor quis tincidunt fringilla, ligula ante consectetur massa, eget semper nulla tellus sit amet sapien. Mauris eget risus laoreet lorem volutpat varius eu lacinia ante. Sed enim dolor, blandit sed pharetra et, hendrerit ac tellus. Nam sed sapien eget lectus condimentum semper eget non purus."
 SAMPLE_TEXT_OUPUT="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec cursus euismod ligula efficitur faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque molestie libero interdum auctor condimentum. Nullam non enim libero. Fusce fermentum lacus ex, non vehicula urna laoreet ut. Aenean at velit odio. Donec blandit imperdiet nunc, et molestie mi tempor at. Mauris dapibus leo augue. In ex ex, interdum ornare auctor sit amet, rhoncus ut ipsum. Integer dictum est non ornare scelerisque. Duis faucibus nisi accumsan, ullamcorper risus et, tincidunt dolor. Fusce laoreet ex purus, eu mattis urna pharetra at. Aliquam nec fermentum eros. Vivamus ac sapien eu metus euismod varius vel sit amet mi. Donec consectetur, tortor quis tincidunt fringilla, ligula ante consectetur massa, eget semper nulla tellus sit amet sapien. Mauris eget risus laoreet lorem volutpat varius eu lacinia ante. Sed enim dolor, blandit sed pharetra et, hendrerit ac tellus. Nam sed sapien eget lectu"
-echo -n ${SAMPLE_TEXT_INPUT} > ${DEVICE_FILE_PATH}
-assert "cat ${DEVICE_FILE_PATH}" "${SAMPLE_TEXT_OUPUT}"
-assert "cat ${DEVICE_FILE_PATH}" ""
+echo -n ${SAMPLE_TEXT_INPUT} > ${INPUT_DEVICE_FILE_PATH}
+#~ assert "cat ${OUTPUT_DEVICE_FILE_PATH}" "${SAMPLE_TEXT_OUPUT}"
+#~ assert "cat ${OUTPUT_DEVICE_FILE_PATH}" ""
 assert_end write_overflow
 
